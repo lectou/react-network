32fc497fa33667df5afe466d3fd428dc9ce4d7c6
Портянко Олександр <lara_57_pr@outlook.com>
2021-03-04 21:21:05 +0200

    news api

--- src/redux/actions/news.js
+++ src/redux/actions/news.js
@@ -8,5 +8,5 @@ const setNews = (news) => ({ type: SET_NEWS, news });
 export const getNewsData = () => async dispatch => {
   dispatch(onLoader(true));
   const news = await newsAPI.getNews();
-  dispatch(setNews(news.articles));
+  dispatch(setNews(news));
 }
